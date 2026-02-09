ee18bdec683b3add425ec8f292058156f005e66b
hulaanmo <inhog.cedrick@gmail.com>
2026-02-09 09:19:18 +0800

    refactor: update app component to implement OnInit and add interval subscription

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('learning-angular-rxjs');
+
+  ngOnInit(): void {
+    interval(1000).subscribe({
+      next: (value) => console.log(value),
+      complete: () => console.log('Do something here'),
+      error: (err) => console.log(err),
+    });
+  }
 }
