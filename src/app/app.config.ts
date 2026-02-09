bd3da269c4e9278928902ed715bf031dbd6c6e2e
hulaanmo <inhog.cedrick@gmail.com>
2026-02-09 12:10:35 +0800

    feat: add zardui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from '@/app.routes';
+import { provideZard } from '@/shared/core/provider/providezard';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideZard(),
   ],
 };
