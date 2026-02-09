db20c98fe84648293b503d13bb457933e2e47b6a
hulaanmo <inhog.cedrick@gmail.com>
2026-02-09 10:21:38 +0800

    refactor: enhance app component with custom observable and signal integration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
-import { interval } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,26 @@ import { interval } from 'rxjs';
 })
 export class App implements OnInit {
   protected readonly title = signal('learning-angular-rxjs');
+  private count = signal(1);
+  private count$ = toObservable(this.count);
+
+  interval$ = interval(1000);
+  intervall = toSignal(this.interval$, { initialValue: 0 });
+
+  private custom$ = new Observable((subscriber) => {
+    subscriber.next('Hello');
+    subscriber.next('World');
+
+    const timer = setTimeout(() => {
+      subscriber.next({ message: 'Value emitted from custom  Observable' });
+      subscriber.complete();
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
 
   ngOnInit(): void {
-    interval(1000).subscribe({
-      next: (value) => console.log(value),
-      complete: () => console.log('Do something here'),
-      error: (err) => console.log(err),
-    });
+    // this.count$.subscribe((data) => console.log(data));
+    this.custom$.subscribe((val) => console.log(val));
   }
 }
