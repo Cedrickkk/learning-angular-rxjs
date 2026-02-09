e0929e6f9d816e9ed1ca8f355147150c507b0785
hulaanmo <inhog.cedrick@gmail.com>
2026-02-09 12:06:40 +0800

    feat: add Todo type definition and clean up app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,35 +1,9 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
 })
-export class App implements OnInit {
-  protected readonly title = signal('learning-angular-rxjs');
-  private count = signal(1);
-  private count$ = toObservable(this.count);
-
-  interval$ = interval(1000);
-  intervall = toSignal(this.interval$, { initialValue: 0 });
-
-  private custom$ = new Observable((subscriber) => {
-    subscriber.next('Hello');
-    subscriber.next('World');
-
-    const timer = setTimeout(() => {
-      subscriber.next({ message: 'Value emitted from custom  Observable' });
-      subscriber.complete();
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  });
-
-  ngOnInit(): void {
-    // this.count$.subscribe((data) => console.log(data));
-    this.custom$.subscribe((val) => console.log(val));
-  }
-}
+export class App {}
